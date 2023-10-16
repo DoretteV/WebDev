fe5348ddbdba946c3d010edf1036d17564f24793
Dorette <dorette.schutte86@gmail.com>
2023-10-16 10:22:42 +0200

    adding code

--- Classwork/chapter19/frontend/src/components/movie.js
+++ Classwork/chapter19/frontend/src/components/movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
@@ -56,6 +57,31 @@ const Movie = (props) => {
             </Card>
             <br></br>
             <h2>Reviews</h2>
+            <br></br>
+            {movie.reviews.map((review, index) => {
+              return (
+                <Media key={index}>
+                  <Media.Body>
+                    <h5>
+                      {review.name + " reviewed on "} {moment(review.date).format("Do MMMM YYYY")}
+                    </h5>
+                    <p>{review.review}</p>
+                    {props.user && props.user.id === review.user_id &&
+                      <Row>
+                        <Col><Link to={{
+                          pathname: "/movies/" +
+                            props.match.params.id +
+                            "/review",
+                          state: { currentReview: review }
+                        }}>Edit</Link>
+                        </Col>
+                        <Col><Button variant="link">Delete</Button></Col>
+                      </Row>
+                    }
+                  </Media.Body>
+                </Media>
+              )
+            })}
           </Col>
         </Row>
       </Container>
