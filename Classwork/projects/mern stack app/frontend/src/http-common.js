810cba3455d60af39b78b7e35aec63d089eb68b4
Dorette <dorette.schutte86@gmail.com>
2023-11-01 10:02:40 +0200

    url

--- Classwork/projects/mern stack app/frontend/src/http-common.js
+++ Classwork/projects/mern stack app/frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://tutorial-cktj.onrender.com/api/tutorials",
+  baseURL: "https://tutorial-hq7w.onrender.com/api/tutorials",
   headers: {
     "Content-type": "application/json"
   }
